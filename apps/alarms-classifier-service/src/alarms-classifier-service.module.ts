26bcd02be7b6dabe654dee74a3b562fe7ba9504f
maks <petuhovmaksim073@gmail.com>
2025-10-08 16:38:25 +0300

    refactor: remove unused service provider from alarms classifier module

--- apps/alarms-classifier-service/src/alarms-classifier-service.module.ts
+++ apps/alarms-classifier-service/src/alarms-classifier-service.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AlarmsClassifierServiceController } from './alarms-classifier-service.controller';
-import { AlarmsClassifierServiceService } from './alarms-classifier-service.service';
 
 @Module({
   imports: [],
   controllers: [AlarmsClassifierServiceController],
-  providers: [AlarmsClassifierServiceService],
+  providers: [],
 })
 export class AlarmsClassifierServiceModule {}
